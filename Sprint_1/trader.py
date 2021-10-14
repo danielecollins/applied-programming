bd1f9b0bdf5fbde79f83609490819bda9f48c819
danielecollins <57637750+danielecollins@users.noreply.github.com>
2021-10-14 13:10:09 -0600

    set up env

--- Sprint_1/trader.py
+++ Sprint_1/trader.py
@@ -4,9 +4,15 @@ from td.client import TDClient
 #Setup for td ameritrade connection
 TDSession = TDClient(
     client_id='12CEWACOWXBGEOPIKQDGPAD1V5QNJU6U',
-    redirect_uri='http://127.0.0.1:5500/Sprint_2/index.html',
+    redirect_uri='http://127.0.0.1:5500/index.html',
     credentials_path='C:\Users\Daniel\Desktop\School\Applied Programming\personal_work\Sprint_1\Credentials'
 )
 
 #login using session credentials
 TDSession.login()
+
+msft_quotes = TDSession.get_quotes(instruments=['MSFT'])
+
+print(msft_quotes)
+
+multiple_quotes = TDSession.get_quotes(instruments=['AMZN','SQ'])
