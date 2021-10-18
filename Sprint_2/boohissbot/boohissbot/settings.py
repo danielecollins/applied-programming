b66fdb9cfdd93c87d573633c8ffc1d8b1070582c
danielecollins <57637750+danielecollins@users.noreply.github.com>
2021-10-18 12:34:13 -0600

    Set up a simple database and the admin user

--- Sprint_2/boohissbot/boohissbot/settings.py
+++ Sprint_2/boohissbot/boohissbot/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Added the stocktrader app
+    'stocktrader.apps.StocktraderConfig',
 ]
 
 MIDDLEWARE = [
--- Sprint_2/boohissbot/stocktrader/models.py
+++ Sprint_2/boohissbot/stocktrader/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Stock(models.Model):
+    ticker = models.CharField(max_length=5)
+    ask = models.DecimalField(max_digits=10, decimal_places=2)
+    bid = models.DecimalField(max_digits=10, decimal_places=2)
+    owned = models.BooleanField() 
+    # when adding a field you need to set it to null=True or add a default value. You can add a default value via the console when you run py manage.py makemigrations
+    # always run py manage.py makemigrations then py manage.py migrate when adding tables
